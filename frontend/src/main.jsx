5eb467a32f71c75bb4270c66eee5b31f3029f29e
androidilicious <diwas.puri@duke.edu>
2026-04-03 17:11:45 -0400

    Add global frontend crash fallback page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import GlobalFallback from './GlobalFallback.jsx'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+  <GlobalFallback>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  ,
+  </GlobalFallback>,
 )
